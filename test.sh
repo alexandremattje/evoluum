f2c3e138b6ff91f1b7b31509e9435a6921dcfda5
Alexandre Mattje <amattje@gmail.com>
2019-08-20 12:42:48 -0300

    sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 curl -s --request POST --header "Content-Type: application/json" --data '{"x":"0", "y":"0", "direction": "N"}' http://localhost:8080/rest/mars/MMRMMRMM
 curl -s --request POST --header "Content-Type: application/json" --data '{"x":"0", "y":"0", "direction": "N"}' http://localhost:8080/rest/mars/MML
 curl -s --request POST --header "Content-Type: application/json" --data '{"x":"0", "y":"0", "direction": "N"}' http://localhost:8080/rest/mars/AAA
